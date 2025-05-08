d87532e36e9021db7f8524c6f10368bc0c56998d
Moaz Ahmed <moaz84211@gmail.com>
2025-05-08 17:42:45 +0300

    JWT Authentication Implementation
    The system currently lacks the ability to authenticate users with a JWT token.

--- lms-backend/src/main/java/net/java/lms_backend/Service/AuthService.java
+++ lms-backend/src/main/java/net/java/lms_backend/Service/AuthService.java
@@ -24,92 +24,76 @@ import static net.java.lms_backend.mapper.UserMapper.ToUserLogin;
 import static net.java.lms_backend.mapper.UserMapper.ToUserRegister;
 
 @Service
+
 public class AuthService {
     private final UserService userService;
     private final ConfirmationTokenService confirmationTokenService;
     private final EmailSender emailSender;
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-//    private final JwtTokenProvider tokenProvider;
-//    private final AuthenticationManager authenticationManager;
-    public AuthService(UserService userService, EmailValidatorService emailValidatorService, ConfirmationTokenService confirmationTokenService, EmailSender emailSender, UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    private final JwtTokenProvider tokenProvider;  // Added JwtTokenProvider
+    private final AuthenticationManager authenticationManager;  // Added AuthenticationManager
+
+    public AuthService(UserService userService, EmailValidatorService emailValidatorService, ConfirmationTokenService confirmationTokenService, EmailSender emailSender, UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, JwtTokenProvider tokenProvider, AuthenticationManager authenticationManager) {
         this.userService = userService;
         this.confirmationTokenService = confirmationTokenService;
         this.emailSender = emailSender;
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-//        this.tokenProvider = tokenProvider;
-//        this.authenticationManager = authenticationManager;
+        this.tokenProvider = tokenProvider;  // Initialize JwtTokenProvider
+        this.authenticationManager = authenticationManager;  // Initialize AuthenticationManager
     }
 
     public ResponseEntity<String> register(RegisterDTO user) {
-        ResponseEntity<String> ret= userService.signUpUser(ToUserRegister(user));
-        if(ret.getStatusCode().equals(HttpStatus.OK)){
-            String token=ret.getBody();
-            String link = "http://localhost:9090/api/auth/confirm?token="+token;
-            emailSender.send(
-                    user.getEmail(),
-                    buildEmail(user.getFirstname(), link));
-            ret= ResponseEntity.status(HttpStatus.CREATED).body("Thank you for registering with us! " +
+        ResponseEntity<String> ret = userService.signUpUser(ToUserRegister(user));
+        if (ret.getStatusCode().equals(HttpStatus.OK)) {
+            String token = ret.getBody();
+            String link = "http://localhost:9090/api/auth/confirm?token=" + token;
+            emailSender.send(user.getEmail(), buildEmail(user.getFirstname(), link));
+            ret = ResponseEntity.status(HttpStatus.CREATED).body("Thank you for registering with us! " +
                     "We're excited to have you onboard.\n" +
                     "To complete your registration, please check your email for the activation " +
                     "link. Click the link in the email to activate your account and start using our services.");
         }
         return ret;
-
     }
 
     @Transactional
     public ResponseEntity<String> confirmToken(String token) {
         ConfirmationToken confirmationToken = confirmationTokenService
                 .getToken(token)
-                .orElseThrow(() ->
-                        new IllegalStateException("link not found"));
+                .orElseThrow(() -> new IllegalStateException("link not found"));
 
         if (confirmationToken.getConfirmedDate() != null) {
-            return  ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
                     .body("User already confirmed");
         }
 
         LocalDateTime expiredDate = confirmationToken.getExpiresDate();
 
         if (expiredDate.isBefore(LocalDateTime.now())) {
-            ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT)
+            return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT)
                     .body("Link expired");
         }
 
         confirmationTokenService.setConfirmedToken(token);
-        userService.enableUser(
-                confirmationToken.getUser().getEmail());
+        userService.enableUser(confirmationToken.getUser().getEmail());
         return ResponseEntity.ok("User successfully confirmed");
     }
+
     private String buildEmail(String name, String link) {
         return "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
-                "\n" +
-                "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
-                "\n" +
+                // HTML email content
                 "  <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;min-width:100%;width:100%!important\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\">\n" +
                 "    <tbody><tr>\n" +
                 "      <td width=\"100%\" height=\"53\" bgcolor=\"#0b0c0c\">\n" +
-                "        \n" +
                 "        <table role=\"presentation\" width=\"100%\" style=\"border-collapse:collapse;max-width:580px\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" align=\"center\">\n" +
                 "          <tbody><tr>\n" +
                 "            <td width=\"70\" bgcolor=\"#0b0c0c\" valign=\"middle\">\n" +
-                "                <table role=\"presentation\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
-                "                  <tbody><tr>\n" +
-                "                    <td style=\"padding-left:10px\">\n" +
-                "                  \n" +
-                "                    </td>\n" +
-                "                    <td style=\"font-size:28px;line-height:1.315789474;Margin-top:4px;padding-left:10px\">\n" +
-                "                      <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Confirm your email</span>\n" +
-                "                    </td>\n" +
-                "                  </tr>\n" +
-                "                </tbody></table>\n" +
-                "              </a>\n" +
+                "              <span style=\"font-family:Helvetica,Arial,sans-serif;font-weight:700;color:#ffffff;text-decoration:none;vertical-align:top;display:inline-block\">Confirm your email</span>\n" +
                 "            </td>\n" +
                 "          </tr>\n" +
                 "        </tbody></table>\n" +
-                "        \n" +
                 "      </td>\n" +
                 "    </tr>\n" +
                 "  </tbody></table>\n" +
@@ -117,20 +101,15 @@ public class AuthService {
                 "    <tbody><tr>\n" +
                 "      <td width=\"10\" height=\"10\" valign=\"middle\"></td>\n" +
                 "      <td>\n" +
-                "        \n" +
-                "                <table role=\"presentation\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
-                "                  <tbody><tr>\n" +
-                "                    <td bgcolor=\"#1D70B8\" width=\"100%\" height=\"10\"></td>\n" +
-                "                  </tr>\n" +
-                "                </tbody></table>\n" +
-                "        \n" +
+                "        <table role=\"presentation\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse\">\n" +
+                "          <tbody><tr>\n" +
+                "            <td bgcolor=\"#1D70B8\" width=\"100%\" height=\"10\"></td>\n" +
+                "          </tr>\n" +
+                "        </tbody></table>\n" +
                 "      </td>\n" +
                 "      <td width=\"10\" valign=\"middle\" height=\"10\"></td>\n" +
                 "    </tr>\n" +
                 "  </tbody></table>\n" +
-                "\n" +
-                "\n" +
-                "\n" +
                 "  <table role=\"presentation\" class=\"m_-6186904992287805515content\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"border-collapse:collapse;max-width:580px;width:100%!important\" width=\"100%\">\n" +
                 "    <tbody><tr>\n" +
                 "      <td height=\"30\"><br></td>\n" +
@@ -138,9 +117,7 @@ public class AuthService {
                 "    <tr>\n" +
                 "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
                 "      <td style=\"font-family:Helvetica,Arial,sans-serif;font-size:19px;line-height:1.315789474;max-width:560px\">\n" +
-                "        \n" +
-                "            <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Hi " + name + ",</p><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> Thank you for registering. Please click on the below link to activate your account: </p><blockquote style=\"Margin:0 0 20px 0;border-left:10px solid #b1b4b6;padding:15px 0 0.1px 15px;font-size:19px;line-height:25px\"><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> <a href=\"" + link + "\">Activate Now</a> </p></blockquote>\n Link will expire in 5 minutes. <p>See you soon</p>" +
-                "        \n" +
+                "        <p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">Hi " + name + ",</p><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> Thank you for registering. Please click on the below link to activate your account: </p><blockquote style=\"Margin:0 0 20px 0;border-left:10px solid #b1b4b6;padding:15px 0 0.1px 15px;font-size:19px;line-height:25px\"><p style=\"Margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\"> <a href=\"" + link + "\">Activate Now</a> </p></blockquote>\n Link will expire in 5 minutes. <p>See you soon</p>" +
                 "      </td>\n" +
                 "      <td width=\"10\" valign=\"middle\"><br></td>\n" +
                 "    </tr>\n" +
@@ -148,7 +125,6 @@ public class AuthService {
                 "      <td height=\"30\"><br></td>\n" +
                 "    </tr>\n" +
                 "  </tbody></table><div class=\"yj6qo\"></div><div class=\"adL\">\n" +
-                "\n" +
                 "</div></div>";
     }
 
@@ -162,17 +138,16 @@ public class AuthService {
             User user = optionalUser.get();
 
             if (bCryptPasswordEncoder.matches(userRequest.getPassword(), user.getPassword())) {
-                if(user.isEnabled()) {
-//                    authenticationManager.authenticate(
-//                            new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
-//                    );
-//
-//                    List<String> roles = new ArrayList<>();
-//                    roles.add(user.getRole().name());
-//
-//                    String token = tokenProvider.generateToken(user.getUsername(), roles);
-//                    return ResponseEntity.ok("Bearer " + token);
-                        return ResponseEntity.ok("Successfully logged in");
+                if (user.isEnabled()) {
+                    authenticationManager.authenticate(
+                            new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
+                    );
+
+                    List<String> roles = new ArrayList<>();
+                    roles.add(user.getRole().name());
+
+                    String token = tokenProvider.generateToken(user.getUsername(), roles);
+                    return ResponseEntity.ok("Bearer " + token);
                 } else {
                     return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User is not enabled");
                 }
@@ -185,5 +160,4 @@ public class AuthService {
                     .body("Invalid Username, Email or Password");
         }
     }
-
 }

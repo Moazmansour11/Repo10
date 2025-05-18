97ea82dd3783def6d1d29071fd43b6f10fe0658a
SeifEldinAmrIsmail <168829870+SeifEldinAmrIsmail@users.noreply.github.com>
2025-05-18 16:27:35 +0300

    instructerDTO

--- lms-backend/src/main/java/net/java/lms_backend/dto/LessonDTO.java
+++ lms-backend/src/main/java/net/java/lms_backend/dto/LessonDTO.java
@@ -1,10 +1,15 @@
 package net.java.lms_backend.dto;
 
 
+import net.java.lms_backend.entity.Lesson;
+
 public class LessonDTO {
     private String title;
 
     private String content;
+
+    public LessonDTO(Lesson savedLesson) {
+    }
 
     public String getTitle() {
         return title;

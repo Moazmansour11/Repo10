4a5bf60aa30180ef5cae5fbe43b2243655be22fe
SeifEldinAmrIsmail <168829870+SeifEldinAmrIsmail@users.noreply.github.com>
2025-05-18 17:12:09 +0300

    instructerDTO

--- lms-backend/src/main/java/net/java/lms_backend/dto/LoginRequestDTO.java
+++ lms-backend/src/main/java/net/java/lms_backend/dto/LoginRequestDTO.java
@@ -10,42 +10,6 @@ public class LoginRequestDTO {
     private String password;
 //add const.
 
-    public LoginRequestDTO(String identifier, String password) {
-        this.identifier = identifier;
-        this.password = password;
-    }
-    public LoginRequestDTO(){
-
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setIdentifier(String identifier) {
-        this.identifier = identifier;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getIdentifier() {
-        return identifier;
-    }
-
-    public void setUsername(String username) {
-        this.identifier = username;
-    }
-    public void setEmail(String email) {
-        this.identifier = email;
-    }
-
-
-
-    public void setPassword(char[] password) {
-        this.password = new String(password);
-    }
-    public String getEmail() {
-        return identifier;
-    }
-
     public String getUsername() {
         return identifier;
     } 
